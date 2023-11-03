013145d17b28d6ea61ec68722987845416aff741
EmSturm <emsturmova@gmail.com>
2023-11-03 13:26:16 +0100

    Základní struktura stránek

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,16 +4,14 @@ export const HomePage = () => {
   return (
     <div className="container">
       <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
+        
       </header>
       <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
+        <h1>Vítejte na stránce o našem projektu</h1>
+        <p>V rámci Ditální Akademie Czechitas jsme vytvořily tento informativní web o našem závěrečnm projektu</p>
+      <button><a href='./pages/AboutProject'>Chci se dozvědět víc o projektu</a></button> <br />
+      <p></p>
+      <button><a href='./pages/AboutUs'>Chci se dozvědět víc o autorkách</a></button>
       </main>
       <footer>
         <p>Czechitas, Digitální akademie: Web</p>
