c3b6e0436988786784a74d0039e025ce0ffdde7e
EmSturm <emsturmova@gmail.com>
2023-11-12 19:30:25 +0100

    Doplnění info do AbouUs

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -11,18 +11,19 @@ export const AboutUs = () => {
         <div className="aboutUs--ema">
           <h1>Ema</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ea
-            blanditiis voluptatem quidem, dolore beatae quibusdam, pariatur
-            quaerat a eligendi unde, reprehenderit possimus nesciunt! Rem sit
-            quis dicta consectetur dolorem est voluptatibus quisquam eius
-            sapiente. Odio, dolore expedita dicta ducimus, ut impedit eius ab
-            molestiae animi provident repudiandae nihil quam in.
+          Ahoj! Jsem Ema máma 2 Kolumbijských synů, co zkouší dívat se na svět neotřelým způsobem. Zkoumám psychologii sociálních bublin a kreativní uspořádání života a webových stránek. Momentálně učím Španělštinu na gymnáziu a uvědomuji si jak kultura formuje a vyživuje naše životy a proto jsem ráda, že jsem se připojila k Justině a projektu, který digitální formou bude promlouvat k dětem a přibližovat jim naše kulturní dědictví.
           </p>
+          <MediaLinks
+            githubLink={'https://github.com/EmSturm'}
+            linkedinLink={
+              'https://www.linkedin.com/in/michala-ema-%C5%A1turmov%C3%A1-a003881a0/'
+            }
+          ></MediaLinks>
         </div>
         <div className="aboutUs--justina">
           <h1>Justina</h1>
           <p>
-            Ahoj! Jsem Justina s stále studuji na vysoké škole, a to konkrétně
+            Ahoj! Jsem Justina a stále studuji na vysoké škole, a to konkrétně
             Arts Management. Mám ráda všechno kreativní a často se učím novým
             věcem. Kultura pro mě vždy byla velmi důležitá a jsem ráda, že i náš
             závěrečný projekt ji bude hravým způsobem propagovat!
